b687ffe2f539ff876c767844c8b712785b5b727f
alex-melia <alexmelia41@gmail.com>
2024-12-16 14:36:23 +0000

    Fix

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -13,6 +13,9 @@ module.exports = {
           "postgresql://postgres.lvquujcrydrprecyklwv:5QfUeqKukxH0UA0P@aws-0-us-east-1.pooler.supabase.com:5432/postgres",
         DIRECT_URL:
           "postgresql://postgres.lvquujcrydrprecyklwv:5QfUeqKukxH0UA0P@aws-0-us-east-1.pooler.supabase.com:5432/postgres",
+        PASSWORD:
+          "$2a$10$qnWDb2.HgxDIJqAhOGYHLOwkVBCx.8iQxMqB7Wbz6619vpzNCAauy",
+        ACCESS_TOKEN_SECRET: "12345",
       },
     },
   ],
